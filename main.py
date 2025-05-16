76376e00e2a5d92d152d6b4cb21331febfd7600d
{{SK}} <shahed.khallaghi@gmail.com>
2025-05-16 10:06:54 +0200

    Update main

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({"☆"})
+    return "☆"
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
