e788d634ec30911a684325c13a6320cfa36579d0
{{SK}} <shahed.khallaghi@gmail.com>
2025-05-16 10:00:20 +0200

    Upload files

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return jsonify({"☆"})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
